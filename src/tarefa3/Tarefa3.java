15cd2e4a33755aff6597fedc25aa26c966826818
Lalo <aalvarezvbretana>
2020-05-09 17:16:51 +0200

    acabado

---
+++ src/tarefa3/Tarefa3.java
@@ -0,0 +1,19 @@
+package tarefa3;
+
+/**
+ *
+ * @author Lalo
+ */
+public class Tarefa3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Ventana vent = new Ventana();
+    vent.setVisible(true);
+    
+    
+    }
+    
+}
